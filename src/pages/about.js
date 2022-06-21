9534de40b81317c2e156e0b5007057cb1bbfa274
Thanh Ngoc Vy <vy.tn@st.buv.edu.vn>
2022-06-21 08:11:46 +0700

    add html spa

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,6 @@ import React from "react";
 import ScrollToTop from "../components/scroll-to-top";
 import SEO from "../components/seo";
 import AboutAward from "../containers/about/about-awards";
-import AboutBannerContainer from "../containers/about/about-banner-container";
-import BrandTwoContainer from "../containers/global/brand-two";
 import SkillContainer from "../containers/global/skill";
 import Layout from "../layouts";
 import Footer from "../layouts/footer";
