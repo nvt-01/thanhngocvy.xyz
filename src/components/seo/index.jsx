a8898926633c9d014d80a15fb54d737e3905619b
Thanh Ngoc Vy <vy.tn@st.buv.edu.vn>
2022-06-23 00:19:21 +0700

    seo

--- src/components/seo/index.jsx
+++ src/components/seo/index.jsx
@@ -8,7 +8,7 @@ const SEO = ({ title }) => {
             <meta charSet="utf-8" />
             <title>{title}</title>
             <meta name="robots" content="noindex, follow" />
-            <meta name="description" content="Thanh Ngoc Vy - Profolio" />
+            <meta name="description" content="Thanh Ngoc Vy - Portfolio" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
